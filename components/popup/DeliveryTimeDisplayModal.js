8918838b75a420e87cb19e7f5396765d1710a3f4
Chrustkiran <christkiran.15@cse.mrt.ac.lk>
2025-05-15 17:15:29 +1000

    pop changed

--- components/popup/DeliveryTimeDisplayModal.js
+++ components/popup/DeliveryTimeDisplayModal.js
@@ -15,10 +15,10 @@ const DeliveryTimeDisplayModal = ({ handleClose, show }) => {
           <Modal.Title className="delivery-modal-title">Delivery Hours: 5:30 PM – 9:30 PM </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          We’re currently accepting delivery orders between <strong>5:30 PM and 9:30 PM</strong> only.
-          Craving something delicious? Order during our delivery window and we’ll bring it hot and fresh to your door!
+          🍽️ We’re currently accepting delivery orders between <strong>5:30 PM and 9:30 PM</strong> only.
+          Craving something delicious? Place your order during our delivery window and we’ll bring it hot and fresh to your door!
           <br></br> <br></br>
-          A $2.99 delivery fee applies to all online orders. You can avoid this fee by ringing us on <a href="tel:01372650894">01372 650894</a> to place your order.
+          💰 <strong>Note: A £2.99</strong> delivery fee applies to all online orders. Want to skip the fee? Just give us a call on <a href="tel:01372650894">01372 650894</a> to place your order.
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
