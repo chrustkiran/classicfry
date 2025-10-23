9faf078a6ee03d74146c3ec728fe52be7dbdc620
Chrustkiran <christkiran.15@cse.mrt.ac.lk>
2025-10-24 00:59:50 +1100

    separated env

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+import dotenv from 'dotenv';
+dotenv.config({ path: `envs/.env.${process.env.APP_ENV || 'local'}` })
+
 const nextConfig = {
   output: "export",
   reactStrictMode: false,
   trailingSlash: true,
-  
+  env: {
+    HOST_URI: process.env.HOST_URI,
+  }
 };
+
 
 export default nextConfig;
